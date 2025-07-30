d0c1d30fe14d1adb01eb902812b207004ab9689f
Sanjam Preet <sanjam.preet@gmail.com>
2025-07-30 16:46:56 -0400

    Wrote code in task_service.py file

--- Task Management System/Service/task_service.py
+++ Task Management System/Service/task_service.py
@@ -1,11 +1,47 @@
-from Models.task import Task
-from Models.queue import Queue
-from Models.stack import Stack
+from Models.Task import Task
+from Models.Queue import Queue
+from Models.Stack import Stack
 from Models.Graph import Graph
 
 class Task_Service:
     def __init__(self):
-        self.tasks=[]
-        self.task_queue=Queue()
-        self.task_histoty=Stack()
         
+        # Initialize an empty list to store all tasks
+        self.tasks=[]
+        
+        # Initialize a queue to manage task execution order (FIFO)
+        self.task_queue=Queue()
+        
+        # Initialize a stack to store completed task history (LIFO)
+        self.task_history=Stack()
+        
+    def create_task(self, id, title, description):
+        
+         # Create a new Task object
+        task=Task(id, title, description)
+        
+         # Add the task to the task list
+        self.tasks.append(task)
+        
+        
+        self.task_queue(task)
+        return task
+    
+    def complete_task(self):
+        
+        # Check if the task queue is not empty
+        if not self.task_queue.is_empty:
+            
+            # Remove the task from the front of the queue
+            task=self.task_queue.dequeue()
+            
+             # Push the completed task into the history stack
+            self.task_history.push(task) # We're pushing task history because it is of Stack DS
+            return task  
+        
+        # If no task is available, return None
+        return None
+    
+    def get_task_history(self):
+        # Return the stack object storing the completed tasks
+        return self.task_history
